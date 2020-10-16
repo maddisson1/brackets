fd4964e1a91359fcb08fc1d05d5d1a8c303cb635
Madina <madina.karabalina@gmail.com>
2020-10-17 00:11:46 +0600

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+//   your solution
+    let brackets = str.split('');
+	newBrackets = [];
+    holder = [];
+	
+	for(let i = 0; i < bracketsConfig.length; i++) {
+		newBrackets[bracketsConfig[i][0]] = bracketsConfig[i][1];
+	}
+
+    for(let i = 0; i < brackets.length; i++) {
+		if(brackets[i] === holder[holder.length - 1]) {
+			holder.pop();
+		} else {
+            if (newBrackets[brackets[i]] !== undefined) { 
+                holder.push(newBrackets[brackets[i]]); 
+            } else {
+                return false;
+		    }
+        }
+    }
+    return holder.length == 0;
 }
